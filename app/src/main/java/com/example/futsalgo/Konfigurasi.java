b64cd9643d20e2b9f438dd35b4da9a1e67681c7e
azizramdan <azizramdan44@gmail.com>
2019-07-19 02:18:20 +0700

    add helper parse date

--- app/src/main/java/com/example/futsalgo/Konfigurasi.java
+++ app/src/main/java/com/example/futsalgo/Konfigurasi.java
@@ -1,9 +1,31 @@
 package com.example.futsalgo;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
 public class Konfigurasi {
     static String HOST = "http://192.168.43.244/futsalgo/";
     public static final String USER = HOST + "user.php";
     public static final String LAPANGAN = HOST + "lapangan.php";
     public static final String PESANAN = HOST + "pesanan.php";
+
+    public static String parseDate(String time, String inputPattern, String outputPattern) {
+
+        SimpleDateFormat inputFormat = new SimpleDateFormat(inputPattern);
+        SimpleDateFormat outputFormat = new SimpleDateFormat(outputPattern, new Locale("id", "ID"));
+
+        Date date = null;
+        String str = null;
+
+        try {
+            date = inputFormat.parse(time);
+            str = outputFormat.format(date);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return str;
+    }
 
 }
